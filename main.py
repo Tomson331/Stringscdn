3cb03919f759ef56e9859fe5dca5899477faff5a
Tomson331 <133116477+Tomson331@users.noreply.github.com>
2023-07-06 12:44:05 +0200

    Wyciąganie fragmentów ze Stringa przy pomocy nawiasów krwaratowych - Slicing.

---
+++ main.py
@@ -0,0 +1,16 @@
+# Day 9 - String cdn
+
+dlugiCiagZnakow = "Nauka Python jest super"
+print(len(dlugiCiagZnakow))
+
+# wyciąganie ze Stringa 6 - 12 znaków
+print("długiCiagZnakow"[6:12])
+
+#Zadanie
+dlugiCiagZnakow ="Phyton jest super"
+print("długiCiagZnakow")
+print(len("dlugiCiagznakow"))
+
+
+
+
